5b27ab260da6f6423374a369ac868e4821987181
libaojie <libaojie@hirisun.com>
2020-11-26 16:42:42 +0800

    使用 LockSupport实现三个线程切换

--- lbj-thread/lbj-thread-12/src/main/java/com/lbj/thread5/ThreadMain5.java
+++ lbj-thread/lbj-thread-12/src/main/java/com/lbj/thread5/ThreadMain5.java
@@ -1,17 +1,25 @@
 package com.lbj.thread5;
 
+import java.util.concurrent.locks.LockSupport;
+
 public class ThreadMain5 {
 
     public static void main(String[] args) {
 
-        Thread thread1 = new Thread5(0);
+        Thread5 thread1 = new Thread5(0);
         thread1.start();
 
-        Thread thread2 = new Thread5(1);
+        Thread5 thread2 = new Thread5(1);
         thread2.start();
 
-        Thread thread3 = new Thread5(2);
+        Thread5 thread3 = new Thread5(2);
         thread3.start();
+
+        thread1.setThread(thread2);
+        thread2.setThread(thread3);
+        thread3.setThread(thread1);
+
+        LockSupport.unpark(thread1);
 
 
     }
