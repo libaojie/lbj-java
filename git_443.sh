35be890f2a65291cf89cc9eb16f936d2872969d2
libaojie <leebaojie@sina.com>
2021-06-01 09:08:52 +0800

    1

--- git_443.sh
+++ git_443.sh
@@ -3,4 +3,6 @@
 v_date=$(date "+%Y-%m-%d_%H_%M_%S")
 
 git config --global http.proxy "localhost:1080"
+git config --global http.proxy 'http://127.0.0.1:1087'
+git config --global https.proxy 'socks://127.0.0.1:1080'
 
